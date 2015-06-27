2602fe890a24276a73ea93b77f29437875b70aab
Amr Hassan <amr.hassan@gmail.com>
2015-06-27 12:01:02 +0200

    DenseVector: Indexing unset values now panics, and more docs.

--- src/densevector.rs
+++ src/densevector.rs
@@ -3,34 +3,34 @@ use std;
 use std::iter::IntoIterator;
 use std::ops::Mul;
 
+/// A vector that stores its values in a contiguous indexed array internally.
 pub struct DenseVector {
     ns: std::vec::Vec<f64>,
-    zero: f64,
 }
 
 impl DenseVector {
 
-    // Size of non-zero elements
+    /// Number of values in this vector
     pub fn size(&self) -> usize {
        self.ns.len()
     }
 
-    // Euclidean length of the vector
+    /// Euclidean length of the vector
     pub fn length(&self) -> f64 {
         self.ns.iter().map(|x| x**x).fold(0.0, |sum, x| sum + x).sqrt()
     }
 
-    // Creates a new instance from an iterator of real numbers
+    /// Creates a new instance from an iterator of real numbers
     pub fn from_iter<T: std::iter::Iterator<Item=f64>>(iter: T) -> DenseVector {
-        DenseVector { ns: iter.collect(), zero: 0.0 }
+        DenseVector { ns: iter.collect() }
     }
 
-    // Creates a DenseVector from any iterable
+    /// Creates a DenseVector from any iterable
     pub fn from<T: IntoIterator<Item=f64>>(x: T) -> DenseVector {
         DenseVector::from_iter(x.into_iter())
     }
 
-    // Returns an iterator over the contained numbers
+    /// Returns an iterator over borrowed contained values
     pub fn iter(&self) -> std::slice::Iter<f64> {
         self.ns.iter()
     }
@@ -58,11 +58,7 @@ impl std::ops::Index<usize> for DenseVector {
     type Output = f64;
 
     fn index(&self, index: usize) -> &f64 {
-        if index < self.ns.len() {
-            &self.ns[index]
-        } else {
-            &self.zero
-        }
+        &self.ns[index]
     }
 }
 
@@ -103,12 +99,6 @@ mod tests {
     fn test_vector_length() {
         let v = DenseVector::from(vec![3.0, 4.0]);
         assert_eq!(5.0, v.length());
-    }
-
-    #[test]
-    fn test_indexing_a_non_set_entry() {
-        let v = DenseVector::from(vec![3.0, 4.0]);
-        assert_eq!(0.0, v[12]);
     }
 
     #[test]
